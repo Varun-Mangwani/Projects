580f72c358b34e1cf786bcc313bec81aa717c2c5
Varun-Mangwani <vpm.media.business@gmail.com>
2025-10-01 17:57:58 +0530

    Add files via upload
    my first few peoject fron sem 1 in c language

---
+++ Area-of-circle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main(){
+    float radius,area;
+    printf("Enter The Radius: ");
+    scanf("%f", &radius);
+
+    area = PI * radius * radius;
+
+    printf("%f",area);
+}
